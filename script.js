f282e19a0e606ee0d240b8b01e014ce6872a9d4e
Matt <matthewcranford314@gmail.com>
2018-01-23 05:43:19 -0800

    Refactor

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
-var one = $("#about").offset();
-var two = $("#portfolio").offset();
-var three = $("#contact").offset();
 
 $(document).ready(function() {
+    var one = $("#about").offset();
+    var two = $("#portfolio").offset();
+    var three = $("#contact").offset();
+    
     $(window).scroll(function(){
       
         var screenPosition = $(document).scrollTop();
         if (screenPosition < two.top) {
-            $( ".one" ).addClass( "active" );
+            $(".one").addClass("active");
             $(".two").removeClass("active");
             $(".three").removeClass("active");
         }
-        if (screenPosition >= two.top) {
-            $( ".two" ).addClass( "active" );
+        if (screenPosition >= two.top -1) {
+            $(".two").addClass("active");
             $(".one").removeClass("active");
             $(".three").removeClass("active");
         }
